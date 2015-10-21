3f58e6a9d51483086ec2ea06b8a891b91b873438
Jack Zhang <Jack Zhang@sha-lri-pc-133.london.lombardrisk.com>
2015-10-21 16:30:33 +0800

    factory

---
+++ factory/readme.md
@@ -0,0 +1,4 @@
+工厂模式  
+1.简单工厂
+例子中的类图  
+![icon]http://github.com/AronGuan/DesignPattern/images/simpleFactory.png
---
+++ factory/src/pers/aron/abstractFactory/Garlic.java
@@ -0,0 +1,8 @@
+package pers.aron.abstractFactory;
+
+public class Garlic implements Veggies {
+
+	public String toString() {
+		return "Garlic";
+	}
+}
