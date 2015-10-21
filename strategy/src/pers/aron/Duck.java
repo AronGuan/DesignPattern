339a5f4600ca66f5d8b08a69c2ca35d52797ba61
Jack Zhang <Jack Zhang@sha-lri-pc-133.london.lombardrisk.com>
2015-10-21 14:26:39 +0800

    strategy

---
+++ strategy/src/pers/aron/Duck.java
@@ -0,0 +1,32 @@
+package pers.aron;
+
+import pers.aron.fly.FlyBehavior;
+import pers.aron.quack.QuackBehavior;
+
+
+public abstract class Duck {
+	protected FlyBehavior flyBehavior;
+	protected QuackBehavior quackBehavior;
+	
+	public abstract void display();
+	
+	public void performFly(){
+		flyBehavior.fly();
+	}
+
+	public void performQuack(){
+		quackBehavior.quack();
+	}
+	
+	public void swim(){
+		System.out.println("All ducks float, even decoys!");
+	}
+
+	public void setFlyBehavior(FlyBehavior flyBehavior) {
+		this.flyBehavior = flyBehavior;
+	}
+
+	public void setQuackBehavior(QuackBehavior quackBehavior) {
+		this.quackBehavior = quackBehavior;
+	}
+}
